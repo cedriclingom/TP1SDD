6bcce565a1f9e74eda18334b3dcaa2ee00039d16
Unknown <cedriclingom@rocketmail.com>
2017-02-20 13:41:15 +0100

    Add functions in listeContigue.c and listeContigue.h

--- listeContigue.c
+++ listeContigue.c
@@ -30,15 +30,58 @@ int rechercherMotif(char * nomAction, char * motif)
 }
 
 
+jour_t ** allocationListeContigue()
+{
+  jour_t ** tab = (jour_t **)malloc(TAILLE_MAX *sizeof(jour_t *));
+  return tab;
+}
 
 
 
- void creerListeJour(semaine_t * pTeteListe, char * motif, jour_t ** tab)
+jour_t * allocationJour()
+{
+  jour_t * pJour = (jour_t *)malloc(sizeof(jour_t));
+  return pJour;
+}
+
+void creerJour(jour_t * pJour, semaine_t * pSemaine, action_t * pAction)
+{
+  strcpy(pJour->ann_sem, pSemaine->ann_sem);
+  strcpy(pJour->jour_heure, pAction->jour_heure);
+}
+
+
+
+void insererJour(jour_t ** pDebut, jour_t*** ppFin, jour_t * pJour,int indice)
+{
+  pDebut[indice] = pJour;
+  *ppFin = *ppFin + 1;
+}
+
+
+
+int lectureMotif(char ** ppMotif)
+{
+  int codeErr = 0, tailleMotif = 0;
+  printf("Veuillez entrer la taille du motif!\n");
+  codeErr = scanf("%d",&tailleMotif);
+  codeErr = 0;
+  *ppMotif = malloc((tailleMotif+1) * sizeof(char));
+  if(*ppMotif != NULL)
+    {
+      printf("Veuillez entrer votre motif!\n");
+      codeErr = scanf("%s",*ppMotif);
+      codeErr = 1;
+    }
+  return codeErr;
+}
+
+void creerListeJour(semaine_t * pTeteListe, char * motif, jour_t ** pDebut, jour_t *** ppFin)
 {
   semaine_t * pcour1 = pTeteListe;
   action_t * pcour2 = NULL;
+  jour_t * pJour = NULL;
   int nbElm=0, motifTrouver=0,tailleMotif=strlen(motif);
-
   while ((nbElm < TAILLE_MAX) && (pcour1 != NULL))
     {
       pcour2 = pcour1->pListeAction;
@@ -47,10 +90,29 @@ int rechercherMotif(char * nomAction, char * motif)
 	  motifTrouver = rechercherMotif(pcour2->nom_action,motif);
 	  if(motifTrouver == tailleMotif)
 	    {
-	      /*insrerMotif dans la liste contigue*/
-	      ++nbElm;
+	      pJour = allocationJour();
+	      if (pJour != NULL)
+		{
+		  creerJour(pJour, pcour1, pcour2);
+		  insererJour(pDebut, ppFin, pJour, nbElm);
+		  ++nbElm;
+		}
 	    }
 	  pcour2 = pcour2->pAction_suiv;
 	}
+      pcour1 = pcour1->pSem_suiv;
     }
+}
+
+
+void libererListeContigue(jour_t ** pDebut, jour_t ** pFin)
+{
+  int i=0;
+  while ((pDebut+i) < pFin)
+    {
+      free(pDebut[i]);
+      ++i;
+    }
+  free(pDebut[i]);
+  free(pDebut);
 }
--- listeContigue.h
+++ listeContigue.h
@@ -21,6 +21,12 @@ typedef struct jour
 
 int compareChaineN (char *, char *, int);
 int rechercherMotif(char *, char *);
-void creerListeJour(semaine_t *, char *, jour_t **);
+jour_t ** allocationListeContigue ();
+jour_t * allocationJour();
+void creerJour(jour_t *, semaine_t *, action_t *);
+void insererJour(jour_t **,jour_t ***, jour_t *,int);
+int lectureMotif(char **);
+void creerListeJour(semaine_t *, char *, jour_t **,jour_t ***);
+void libererListeContigue(jour_t **, jour_t **);
 
 #endif
